dc811617fcdc2cc0c02747cc31df2054e228068a
elena <elena.valenzuela.guerrero@gmail.com>
2022-04-16 15:57:19 +0200

    Add way to check when we are finished WIP

--- js/main.js
+++ js/main.js
@@ -12,4 +12,38 @@ function checkCoincidences(fixedWord, attemptedWord) {
   return [MATCH, FAIL, MATCH, MATCH, MATCH, FAIL];
 }
 
-checkCoincidences('patata', 'petate');
+function areEquals(array1, array2) {
+  return (
+    array1.length === array2.length &&
+    array1.every(function (value, index) {
+      return value === array2[index];
+    })
+  );
+}
+
+console.log(checkCoincidences('patata', 'petate'));
+console.log(checkCoincidences('anexo', 'tapiz'));
+console.log(checkCoincidences('anexo', 'rodar'));
+console.log(checkCoincidences('anexo', 'joder'));
+console.log(checkCoincidences('anexo', 'albeo'));
+console.log(checkCoincidences('anexo', 'ameno'));
+console.log(checkCoincidences('anexo', 'anexo'));
+
+console.log('------ resultados ------');
+
+console.log(
+  areEquals(checkCoincidences('patata', 'petate'), [
+    MATCH,
+    FAIL,
+    MATCH,
+    MATCH,
+    MATCH,
+    FAIL,
+  ])
+);
+console.log(checkCoincidences('anexo', 'tapiz'));
+console.log(checkCoincidences('anexo', 'rodar'));
+console.log(checkCoincidences('anexo', 'joder'));
+console.log(checkCoincidences('anexo', 'albeo'));
+console.log(checkCoincidences('anexo', 'ameno'));
+console.log(checkCoincidences('anexo', 'anexo'));
